4752bc9954d0ab9bd8b9d194647f2d57115ffeb7
drlippman <drlippman@yahoo.com>
2017-01-06 00:58:42 -0800

    Hide the logo / dropdown course menu on moveitem dialog

--- course/moveitem.php
+++ course/moveitem.php
@@ -166,6 +166,7 @@ if ($itemtomove{0} != 'B') {
 }
 
 $flexwidth = true;
+$nologo = true;
 $placeinhead = '<script type="text/javascript" src="'.$imasroot.'/javascript/moveitem.js"></script>';
 $placeinhead .= '<script type="text/javascript">
   var blockinfo = '.json_encode($briefitems).';
